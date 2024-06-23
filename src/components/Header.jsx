7ff096586787f9efa87567c66eeaf804ccfb2a4e
simonjoseph <simaojose1978@gmail.com>
2024-06-23 08:38:18 +0100

    header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import './Header.module.css'
+import styles from './Header.module.css'
 import Logo from '../assets/Logo.png' 
 
 export function Header(){
     return( 
-        <header>
+        <header className={styles.header} >
             <img src={Logo} alt="" />
         </header>
         )
